b33151ec7d1ee43fef9ebb203afa0d39e9bad785
Yuliap21 <yulia777best@gmail.com>
2021-07-13 00:30:21 -0400

    Tip Calculator express application created

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const express = require ('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.get('/greeting/', (req, res) => {
-  res.send('Hello, Stranger');
+  res.send('<h1>Hello, Stranger</h1>');
 });
 
-
-app.get('/hello/:name', (req, res) => {
-	console.log(req.params);
-	res.send('Hello ' + req.params.name);
+app.get('/hello/:firstName/:lastName', (req, res) => {
+  console.log(req.params);
+  res.send(`hello ${req.params.firstName} ${req.params.lastName}`)
 });
 
+app.get("/",(req, res) =>{
+	res.send("hello! please type tip")
+});
+//
+// app.get('/tip/:total/:tipPercentage', (req, res) =>{
+//   tip = (req.params.tip); // total *
+//   total = parseInt(req.params.total);
+//   tipPercentage = parseInt(req.params.tipPercentage);
+//   res.send(tip);
+// });
 
 
 
