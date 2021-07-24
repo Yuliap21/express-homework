dbb9bf08f3ca7439faedd61c6b1cfbaeeec27e76
Yuliap21 <yulia777best@gmail.com>
2021-07-24 17:43:50 -0400

    my tip calculation is done

--- server.js
+++ server.js
@@ -13,30 +13,41 @@ const responses = ["It is certain", "It is decidedly so", "Without a doubt", "Ye
 //   res.send(`hello ${req.params.firstName} ${req.params.lastName}`)
 // });
 
-// app.get("/",(req, res) =>{
-// 	res.send("hello! please type tip")
-// });
-//
-// app.get('/tip/:total/:tipPercentage', (req, res) =>{
-//   tip = (req.params.tip); // req.params.total : 100 * req.params.tipPercentage
+app.get("/",(req, res) =>{
+	res.send("hello! please type tip")
+});
+
+app.get('/tip/:total/:tipPercentage', (req, res) =>{
+  let tip;
+  const total = parseInt(req.params.total);
+  const tipPercentage = parseFloat(req.params.tipPercentage /100);
+  tip = total * tipPercentage ;
+  res.send(tip.toString());
+});
 
 
-//   total = parseInt(req.params.total);
-//   tipPercentage = parseInt(req.params.tipPercentage);
-//   res.send(tip);
-// });
+
+
 app.get('/magic', (req, res) =>{
-  res.send(` <h1> [''] </h1>
+  res.send(` <h1>  </h1>
     'Will I be a Millionaire?'`);
 });
+
+// app.get('/magic/Will%20I%20Be%20A%20Millionaire', (req, res) =>{
+//   res.send(["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]);
+//
+// });
 
 app.get('/:indexOfResponsesArray', (req, res) =>{
   res.send(responses[req.params.indexOfResponsesArray]);
 });
 
-// app.get('/magic', (req, res) =>{
-//   res.redirect('/magic');
-// });
+app.get('/magic', (req, res) =>{
+  res.redirect('/magic');
+});
+
+
+
 
 app.listen(PORT,() =>{
   console.log('We are in the building', PORT)
